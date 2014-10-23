0ab4ab80e6110273afe5d8a5267a6eeb0d4dfbf5
Neale Swinnerton <neale@isismanor.com>
2014-10-23 19:04:43 +0100

    Add url and license info to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject mastodonc/clj-kafka "0.2.6-0.8.1.1"
+  :url "https://github.com/MastodonC/clj-kafka"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [zookeeper-clj "0.9.3"]
